b35b3fe483413b24acb58079f39270a4f852cefa
Zhengxinyang <31161448+Zhengxinyang@users.noreply.github.com>
2021-06-16 17:02:30 +0800

    Rect

--- src/utils.h
+++ src/utils.h
@@ -11,80 +11,84 @@
 
 // uniform random number in [0, 1]
 inline float rnd() {
-  static std::mt19937 mt(0);
-  static std::uniform_real_distribution<float> dist(0, 1);
-  return dist(mt);
+    static std::mt19937 mt(0);
+    static std::uniform_real_distribution<float> dist(0, 1);
+    return dist(mt);
 }
 
 // override sf::Vector2f to satisfy PointT requirements
 class Point2f : public sf::Vector2f {
- public:
-  static constexpr unsigned int dim = 2;
+public:
+    static constexpr unsigned int dim = 2;
 
-  Point2f(float x, float y) : sf::Vector2f(x, y) {}
-  template <typename T>
-  Point2f(const sf::Vector2<T>& point) : sf::Vector2f(point) {}
+    Point2f(float x, float y) : sf::Vector2f(x, y) {}
+    template <typename T>
+    Point2f(const sf::Vector2<T>& point) : sf::Vector2f(point) {}
 
-  float operator[](unsigned int i) const {
-    if (i == 0) {
-      return x;
-    } else if (i == 1) {
-      return y;
-    } else {
-      std::cerr << "invalid dimension" << std::endl;
-      std::exit(EXIT_FAILURE);
+    float operator[](unsigned int i) const {
+        if (i == 0) {
+            return x;
+        } else if (i == 1) {
+            return y;
+        } else {
+            std::cerr << "invalid dimension" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
     }
-  }
 };
 
 // ball entity
 class Ball : public sf::Drawable {
- protected:
-  sf::Vector2f position;
-  float radius;
-  sf::CircleShape circle;
+protected:
+    sf::Vector2f position;
+    float radius;
+    sf::CircleShape circle;
 
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    target.draw(circle);
-  }
-
- public:
-  static constexpr unsigned int dim = 2;
-
-  Ball(const sf::Vector2f& position, float radius) {
-    setRadius(radius);
-    setPosition(position);
-    circle.setFillColor(sf::Color::Transparent);
-    circle.setOutlineThickness(1.0);
-    circle.setOutlineColor(sf::Color::Black);
-  }
-
-  float operator[](unsigned int i) const {
-    if (i == 0) {
-      return position.x;
-    } else if (i == 1) {
-      return position.y;
-    } else {
-      std::cerr << "invalid dimension" << std::endl;
-      std::exit(EXIT_FAILURE);
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        target.draw(circle);
     }
-  }
 
-  sf::Vector2f getPosition() const { return position; }
-  float getRadius() const { return radius; }
+public:
+    static constexpr unsigned int dim = 2;
 
-  void setPosition(const sf::Vector2f& position) {
-    this->position = position;
-    circle.setPosition(position - sf::Vector2f(radius, radius));
-  }
+    Ball(const sf::Vector2f& position, float radius) {
+        setRadius(radius);
+        setPosition(position);
+        circle.setPointCount(300); //为了让圆更圆滑
+        circle.setFillColor(sf::Color::Transparent);
+        circle.setOutlineThickness(1.0);
+        circle.setOutlineColor(sf::Color::Black);
+    }
 
-  void setRadius(float r) {
-    radius = r;
-    circle.setRadius(r);
-  }
+    float operator[](unsigned int i) const {
+        if (i == 0) {
+            return position.x;
+        } else if (i == 1) {
+            return position.y;
+        } else {
+            std::cerr << "invalid dimension" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+    }
 
-  void setColor(const sf::Color& color) { circle.setOutlineColor(color); }
-};/*
+    sf::Vector2f getPosition() const { return position; }
+    float getRadius() const { return radius; }
+
+    void setPosition(const sf::Vector2f& position) {
+        this->position = position;
+        circle.setPosition(position - sf::Vector2f(radius, radius));
+    }
+
+    void setRadius(float r) {
+        radius = r;
+        circle.setRadius(r);
+    }
+
+    void setColor(const sf::Color& color) { circle.setOutlineColor(color); }
+};
+
+
+/*
 * @File: utils.h
 * @Date:   2021-06-16 08:53:58
 * @Last Modified time: 2021-06-16 08:53:58
